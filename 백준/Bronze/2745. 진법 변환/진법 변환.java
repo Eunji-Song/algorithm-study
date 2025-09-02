643f3c459f38e8eb735cd4b3c864e5a773ec0b91
Eunji <66210283+Eunji-Song@users.noreply.github.com>
2025-09-02 23:38:02 +0900

    [Bronze II] Title: 진법 변환, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,27 +1,17 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String N = st.nextToken().trim();
+
+        String N = st.nextToken();
         int B = Integer.parseInt(st.nextToken());
 
-        long ans = 0;
-        for (int i = 0; i < N.length(); i++) {
-            char ch = N.charAt(i);
-            int digit;
-            if ('0' <= ch && ch <= '9') {
-                // 숫자 처리 
-                digit = ch - '0';
-            } else { 
-                // 알파벳 처리 
-                digit = ch - 'A' + 10;
-            }
-            
-            ans = ans * B + digit;
-        }
+        // 내장 메서드로 진법 변환
+        int ans = Integer.parseInt(N, B);
         System.out.println(ans);
     }
 }
